95b567e67f5b17561668a5cf4a700cc0473dd419
Sayd Karam <onltrabalho@gmail.com>
2024-05-15 16:45:45 -0300

    refactor: :art: finish graphics

---
+++ src/components/graphics/barChar.tsx
@@ -0,0 +1,23 @@
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+
+interface barChartType {
+    data: [any];
+    datakey?: string;
+    datekey?: string;
+}
+
+
+const barChart: React.FC<barChartType> = ({ data, datakey, datekey }) => {
+    return (
+        <BarChart width={480} height={300} data={data}>
+            <Legend />
+            <XAxis dataKey={datekey} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar type="monotone" dataKey={datakey} stroke="#8884d8" fill="#8884d8" />
+        </BarChart>
+    )
+}
+
+export default barChart
